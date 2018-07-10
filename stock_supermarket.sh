f85d7f29e5ded810384e52e6977800e20506fffd
katiejg <jng.katie@gmail.com>
2018-07-10 17:36:38 +0000

    automatically backed up by learn

---
+++ stock_supermarket.sh
@@ -0,0 +1,33 @@
+mv apples.txt ../fruit;
+mv bison_burgers.txt ../meat;
+mv brie.txt ../dairy;
+mv broccoli.txt ../veggies;
+mv carrot_cake.txt ../dessert;
+mv carrots.txt ../veggies;
+mv eggo_waffles.txt ../freezer;
+mv fage_yogurt.txt ../dairy;
+mv fig_bars.txt ../;
+mv flourless_cake.txt ../dessert;
+mv garlic.txt ../;
+mv goldfish.txt;
+mv grapples.txt; 
+mv hotdogs.txt;
+mv icecream.txt;
+mv lamb_ribs.txt;
+mv land_o_lakes_butter.txt;
+mv lean_cuisine.txt;
+mv leeks.txt;
+mv milk.txt;
+mv mint_milano_cookies.txt;
+mv onion.txt;
+mv oreos.txt;
+mv potatoes.txt;
+mv salmon_steak.txt;
+mv tiramisu_cake.txt;
+mv tomatoes.txt;
+mv trail_mix.txt;
+mv turkey_breast.txt;
+mv velveeta_cheese.txt
+mv watermelon.txt
+mv whole_chicken.txt
+mv wonder_bread.txt
